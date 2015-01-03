f7bff7f9ed67aebd5308013f4c0bb7636482037d
iamianami <ian751029@gmail.com>
2015-01-03 20:04:50 +0800

    unfishned with AI

--- tictactoe.rb
+++ tictactoe.rb
@@ -6,6 +6,7 @@
 #4.check who is winner,if there is no winner or no empty square,loop the game
 #5.if there is a winner,end the loop
 
+
 def initialize_board
   board = {}                                     #board is a empty hash 
   (1..9).each {|position| board[position] = " "}
@@ -23,6 +24,14 @@ def player_place_pices(board) #is the "board" in parameter meas the "board" from
   end until empty_pices(board).include?(position)  
     board[position] = "X"
 end
+################Try to figure it out#################
+# def two_in_row(position,mark)
+#   winner_line = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+#   winner_line.each do |line|
+#     if line.each.include?(mark) == 2
+#       position = 
+# end
+###############Try to figure it out###################
 
 def computer_place_pices(board) #is the "board" in parameter meas the "board" from the "board = initialize_board"?
   position = empty_pices(board).sample
@@ -56,20 +65,27 @@ def annouce_winner(winner)
   puts "#{winner} won!"
 end
 
-board = initialize_board
-draw_board(board)
-begin
-  player_place_pices(board)
-  draw_board(board)
-  computer_place_pices(board)
-  draw_board(board)
-  winner = check_winner(board)
-end until winner || all_pices_filled(board)
 
-if winner
- annouce_winner(winner)
-else
-  puts "it's tie!"
+
+loop do
+  board = initialize_board
+  draw_board(board)
+  begin
+    player_place_pices(board)
+    draw_board(board)
+    computer_place_pices(board)
+    draw_board(board)
+    winner = check_winner(board)
+  end until winner || all_pices_filled(board)
+
+  if winner
+   annouce_winner(winner)
+  else
+    puts "it's tie!"
+  end
+
+  puts "Want to play again?  (Y/N)"
+  answer = gets.chomp.downcase
+  break if answer == "n"
 end
-
 
